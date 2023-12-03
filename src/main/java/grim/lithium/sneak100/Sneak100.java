56806867e4bd484b08fac473eb9bb58a52243c1c
Steveplays28 <djspaargaren@outlook.com>
2023-12-03 16:49:28 +0100

    fix: Fix Gradle and mod initializer
    Also fixed the Fabric API version.

--- src/main/java/grim/lithium/sneak100/Sneak100.java
+++ src/main/java/grim/lithium/sneak100/Sneak100.java
@@ -1,2 +1,16 @@
-public class Sneak100 {
+package grim.lithium.sneak100;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sneak100 implements ModInitializer {
+	public static final String MOD_ID = "sneak100";
+	public static final String MOD_NAME = "Sneak 100";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Loading {}.", MOD_NAME);
+	}
 }
